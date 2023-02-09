081873c0f861ce6016adfa3e667a70c205d1a242
YongA <ji5919@naver.com>
2023-02-09 11:20:36 +0900

    Q_My Todo List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([
+    { id: 1, title: "react를 배워봅시다." },
+    { id: 2, title: "useState를 배워봅시다." },
+    { id: 3, title: "자바스크립트를 배워봅시다." },
+  ]);
+
+  const [title, setTitle] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // 추가 버튼 클릭
+  const clickAddButtonHandler = (event) => {
+    event.preventDefault();
+    const newUser = {
+      id: users.length + 1,
+      title: title,
+    };
+    setUsers([...users, newUser]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form action="">
+      <div>
+        <div className="input-style">
+          <input
+            value={title}
+            onChange={titleChangeHandler}
+            autoFocus
+            placeholder="자바스크립트를 배워봅시다."
+          />
+          <button onClick={clickAddButtonHandler}>추가하기</button>
+        </div>
+        <div className="header-style">
+          <b>Todo List</b>
+        </div>
+        <div className="app-style">
+          {users.map((item) => {
+            console.log(item);
+            return (
+              <div key={item.id} className="component-style">
+                {item.title}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </form>
   );
-}
+};
 
 export default App;
